434bd6505186440726463b2b2020fe4ff3f0a3c3
song7749 <song7749@homeplus.co.kr>
2015-04-28 15:37:54 +0900

    member controller add

---
+++ src/main/java/com/song7749/app/dbclient/controller/MemberController.java
@@ -0,0 +1,25 @@
+package com.song7749.app.dbclient.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * <pre>
+ * Class Name : MemberController.java
+ * Description : 회원 컨트롤러
+ *
+ *  Modification Information
+ *  Modify Date 		Modifier	Comment
+ *  -----------------------------------------------
+ *  2015. 4. 27.		song7749	신규작성
+ *
+ * </pre>
+ *
+ * @author song7749
+ * @since 2015. 4. 27.
+ */
+@Controller
+@RequestMapping("/member")
+public class MemberController {
+
+}
