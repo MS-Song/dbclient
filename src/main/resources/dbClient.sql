15bb3a8bd227c181d951421302b3323421a1af39
minsoo Song <song7749@gmail.com>
2016-01-28 17:45:47 +0900

    SQL 파일 수정

--- src/main/resources/dbClient.sql
+++ src/main/resources/dbClient.sql
@@ -1,9 +1,5 @@
 insert into Member (email, password, passwordAnswer, passwordQuestion, id,authType) values ( 'song7749@gmail.com', '12345678', 'password question', 'password answer', 'root', 'ADMIN' );
 		
-insert into ServerInfo (account, charset, driver, host, hostAlias, password, port, schemaName) values ('dbclient','UTF-8','mysql','127.0.0.1','local_Mysql','1234','3306','dbclient');
-insert into ServerInfo (account, charset, driver, host, hostAlias, password, port, schemaName) values ('STYLE','UTF-8','oracle','10.10.13.211','GMO_TEST','STYLE','1521','TESTWEB');
-insert into ServerInfo (account, charset, driver, host, hostAlias, password, port, schemaName) values ('GHSADM','UTF-8','oracle','10.10.26.123','GHS_TEST_Aliance','GHSADM','1521','GHSTEST');
-
 
 
 
