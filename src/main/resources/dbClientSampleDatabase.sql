a65e78644c48b2cb6903d95f9ec3109417954a85
song7749 <song7749@gmail.com>
2017-02-21 14:13:38 +0900

    sql sample change

--- src/main/resources/dbClientSampleDatabase.sql
+++ src/main/resources/dbClientSampleDatabase.sql
@@ -1,1 +1,1 @@
-INSERT INTO `ServerInfo` (serverInfoSeq,account,charset,driver,host,hostAlias,password,port,schemaName) VALUES (1,'dbclient','UTF-8','mysql','127.0.0.1','local_Mysql','1234','3306','dbclient'),(2,'STYLE','UTF-8','oracle','10.10.40.111','GMO_TEST','STYLE','1661','GMOTST'),(3,'GHSADM','UTF-8','oracle','10.10.13.212','GHS_TEST','ghs0515adm','1521','GHSTST');
+INSERT INTO `ServerInfo` (serverInfoSeq,account,charset,driver,host,hostAlias,password,port,schemaName) VALUES (1,'dbclient','UTF-8','mysql','127.0.0.1','local_Mysql','1234','3306','dbclient');
