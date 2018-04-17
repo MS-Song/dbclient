5738fa2317b1d85606f1f2fff9766b5b9400f2ca
song7749@gmail.com <song7749@gmail.com@local-dev>
2018-04-17 13:03:55 +0900

    swagger config 변경

--- src/main/java/com/song7749/dbclient/web/config/SwaggerConfig.java
+++ src/main/java/com/song7749/dbclient/web/config/SwaggerConfig.java
@@ -33,7 +33,7 @@ public class SwaggerConfig {
 				.description("지정된 조건이 발생하면, 대상자에게 Alert 을 보내는 서비스")
 				.version("1.0.0")
 				.contact(new Contact("Minsoo Song"
-						,"https://github.com/MS-Song/InsidentAlert.git"
+						,"https://github.com/MS-Song/dbclient"
 						,"song7749@gmail.com"))
 				.build();
 	}
