e458d631ac83b53d7aeb64d9a9af9a2ba60863a6
song7749@gmail.com <song7749@gmail.com>
2018-06-08 13:20:42 +0900

    init class add

--- src/main/java/com/song7749/web/config/InitConfigBean.java
+++ src/main/java/com/song7749/web/config/InitConfigBean.java
@@ -3,8 +3,6 @@ package com.song7749.web.config;
 import static com.song7749.util.LogMessageFormatter.format;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -20,8 +18,6 @@ import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.song7749.base.SendMethod;
-import com.song7749.base.YN;
 import com.song7749.dbclient.domain.Database;
 import com.song7749.dbclient.domain.Member;
 import com.song7749.dbclient.repository.DatabaseRepository;
@@ -36,9 +32,6 @@ import com.song7749.dbclient.value.ExecuteQueryDto;
 import com.song7749.dbclient.value.MemberVo;
 import com.song7749.incident.repository.IncidentAlarmRepository;
 import com.song7749.incident.service.IncidentAlarmManager;
-import com.song7749.incident.value.IncidentAlarmAddDto;
-import com.song7749.incident.value.IncidentAlarmConfirmDto;
-import com.song7749.incident.value.IncidentAlarmVo;
 
 /**
  * <pre>
@@ -174,37 +167,5 @@ public class InitConfigBean {
 				}
 			}
 		}
-
-		// incident Alert 의 job 을 하나 등록 한다.(Log 삭제)
-		List<Long> memberIds = new ArrayList<Long>();
-		memberIds.add(member.getId());
-		memberIds.add(member.getId());
-		memberIds.add(member.getId());
-
-		IncidentAlarmAddDto dto = new IncidentAlarmAddDto(
-				"테스트 모니터링",
-				"select 'Y' execute from dual",
-				"select * from database",
-				SendMethod.EMAIL,
-				YN.Y,
-				"*/30 * * * * *",
-				db.getId(),
-				member.getId(),
-				memberIds);
-		IncidentAlarmVo vo = incidentAlarmManager.addIncidentAlarm(dto);
-
-		IncidentAlarmConfirmDto confirmDto = new IncidentAlarmConfirmDto(
-				vo.getId(),
-				YN.Y,
-				new Date(System.currentTimeMillis()),
-				member.getId());
-
-		incidentAlarmManager.confirmIncidentAlarm(confirmDto);
-
-//		// 테스트를 위한 대량 등록
-//		for(int i=0;i<50;i++) {
-//			dto.setSubject(dto.getSubject()+i);
-//			incidentAlarmManager.addIncidentAlarm(dto);
-//		}
     }
 }
