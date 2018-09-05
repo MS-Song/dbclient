30751622b36568108bc4f1bf1420f9f3fea42699
song7749@gmail.com <song7749@gmail.com>
2018-09-05 18:13:48 +0900

    mailconfig hashcode add

--- src/main/java/com/song7749/mail/domain/MailConfig.java
+++ src/main/java/com/song7749/mail/domain/MailConfig.java
@@ -296,4 +296,29 @@ public class MailConfig {
 	public void setModifyDate(Date modifyDate) {
 		this.modifyDate = modifyDate;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MailConfig other = (MailConfig) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 }
