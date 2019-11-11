80a56d42f42f77c5a8d1d30a62bca94111d058db
송민수 <song7749@gmail.com>
2019-11-11 13:55:21 +0900

    IU-2019.2.4 <songminsu@songminsuui-MacBookPro.local Create ide.general.xml, project.default.xml, docker.xml, Class.java, jdk.table.xml, Default.xml, Custom.xml, ignore.xml, find.xml, ui.lnf.xml, laf.xml, web-browsers.xml, filetypes.xml, vcs.xml, colors.scheme.xml, keymap.xml, _@user_Darcula.icls, path.macros.xml, databaseDrivers.xml, editor.xml, github.xml, templates.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,10 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+
+/**
+ * User: song7749
+ * Date: ${DATE}
+ * 
+ */
+public class ${NAME} {
+}
